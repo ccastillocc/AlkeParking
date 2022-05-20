a56ae8ed9a49fd34dee2372490df5d7e9fa87811
Johanna Goenaga <johannagoenagac@gmail.com>
2022-05-20 17:11:21 -0400

    Cambio color

--- app/src/main/java/com/example/alkeparking/Vehicle.kt
+++ app/src/main/java/com/example/alkeparking/Vehicle.kt
@@ -2,7 +2,7 @@ package com.example.alkeparking
 
 data class Vehicle(val plate: String){
 
-    var color = ""
+    var color = "rojo"
     override fun equals(other: Any?): Boolean {
         if (other is Vehicle) {
                 return this.plate == other.plate
