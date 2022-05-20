78990be6bf0e455507b09685545be9950d361e27
Carlos <ccastillom@outlook.cl>
2022-05-20 16:39:52 -0400

    Nueva clase

--- app/src/main/java/com/example/alkeparking/Vehicle.kt
+++ app/src/main/java/com/example/alkeparking/Vehicle.kt
@@ -2,7 +2,7 @@ package com.example.alkeparking
 
 data class Vehicle(val plate: String){
 
-    var chassis = 0
+    var color = ""
     override fun equals(other: Any?): Boolean {
         if (other is Vehicle) {
                 return this.plate == other.plate
