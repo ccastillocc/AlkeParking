8ed0ecf5ed4cc6c28fe960ad94643a1f0c401727
Johanna Goenaga <johannagoenagac@gmail.com>
2022-05-21 20:50:05 -0400

    Add type and rate

--- app/src/main/java/com/example/alkeparking/VehicleType.kt
+++ app/src/main/java/com/example/alkeparking/VehicleType.kt
@@ -1,4 +1,16 @@
 package com.example.alkeparking
 
-enum class VehicleType {
+enum class VehicleType(var type:String, val rate:Int){
+    AUTO("auto", 20),
+    MOTO("moto", 15),
+    MINIBUS ("minibus", 25),
+    BUS("bus", 30)
 }
+
+
+
+
+
+
+
+
