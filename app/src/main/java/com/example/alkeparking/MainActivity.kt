b0abea3cd66438aa3f430f2f42e005e373952f0f
Carlos <ccastillom@outlook.cl>
2022-05-23 18:06:08 -0400

    Nueva clase

--- app/src/main/java/com/example/alkeparking/MainActivity.kt
+++ app/src/main/java/com/example/alkeparking/MainActivity.kt
@@ -14,18 +14,21 @@ class MainActivity : AppCompatActivity() {
         val moto = Vehicle( "BXFT44", "MOTORCYCLE", Calendar.getInstance(), null)
         val minibus = Vehicle( "AADC63", "MINIBUS", Calendar.getInstance(), null)
         val bus = Vehicle( "AHBF45", "BUS", Calendar.getInstance(), "DISCOUNT_CARD_002")
-        val parking = Parking(mutableSetOf(car, moto, minibus, bus))
+        val parking = Parking(mutableSetOf(car, moto, minibus, bus),20)
 
-        println(parking.vehicles.contains(car))
-        println(parking.vehicles.contains(moto))
-        println(parking.vehicles.contains(minibus))
-        println(parking.vehicles.contains(bus))
+        //EJERCICIO 5
+        //println(parking.vehicles.contains(car))
+        //println(parking.vehicles.contains(moto))
+        //println(parking.vehicles.contains(minibus))
+        //println(parking.vehicles.contains(bus))
 
-        //val car2 = Vehicle( "AASE67", "CAR", Calendar.getInstance(), "DISCOUNT_CARD_003")
-        //val isCarInserted = parking.vehicles.add(car2)
-        //println(isCarInserted)
+        val car2 = Vehicle( "AASE67", "CAR", Calendar.getInstance(), "DISCOUNT_CARD_003")
+        //val isCarInserted = parking.addVehicle(car2)
+        println(parking.addVehicle(car2))
         //parking.vehicles.remove(moto)
         //println(parking.vehicles.size)
+
+
     }
 
     
--- app/src/main/java/com/example/alkeparking/Parking.kt
+++ app/src/main/java/com/example/alkeparking/Parking.kt
@@ -1,6 +1,17 @@
 package com.example.alkeparking
 
-data class Parking(var vehicles : MutableSet<Vehicle>){
 
+
+data class Parking(var vehicles : MutableSet<Vehicle>, var cupo : Int){
+
+     fun addVehicle(vehicle : Vehicle): String{
+
+         val message = when{
+             vehicles.size >= cupo -> "Sorry, the check-in failed"
+             vehicles.add(vehicle) -> "Welcome to AlkeParking!"
+             else -> "Sorry, the has check-in failed"
+         }
+         return message
+    }
 
 }
