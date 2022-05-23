90d54d5a3ac1419f1ccda23513afc9c7cfdc5247
Carlos <ccastillom@outlook.cl>
2022-05-23 16:09:29 -0400

    Nueva clase

--- app/src/main/java/com/example/alkeparking/MainActivity.kt
+++ app/src/main/java/com/example/alkeparking/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.alkeparking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.alkeparking.Parking
+import com.example.alkeparking.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val parking : Parking? = null
+
+
 
     }
 }
